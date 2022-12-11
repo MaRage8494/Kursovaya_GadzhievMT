5665995dcec55ebaaeb62cd519c6082ec20978d8
MaRage8494 <marat_gadzhiev_2003@mail.ru>
2022-12-11 18:01:04 +0300

    Update center.js

--- scripts/center.js
+++ scripts/center.js
@@ -2,10 +2,8 @@ let textBlock = document.querySelector(".video");
 let text = document.querySelector(".video .video_title");
 
 function textToCenter(text) {
-    text.style.left = (text.parentElement.clientWidth - text.clientWidth) / 5  + "px";
+    text.style.left = (text.parentElement.clientWidth - text.clientWidth) / 4  + "px";
     text.style.top = (text.parentElement.clientHeight - text.clientHeight) / 1.5 + "px";
-    console.log(text.parentElement.clientWidth);
-    console.log(text.parentElement.clientHeight);
 }
 
 textToCenter(text);
